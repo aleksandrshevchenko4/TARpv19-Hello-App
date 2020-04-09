15912c6ef833966dfc62badcfecc5df94a64a63c
aleksandr.shevchenko <sanjkashevcenko@gmail.com>
2020-04-09 11:29:26 +0300

    Use string interpolation

--- TARpv19/Program.cs
+++ TARpv19/Program.cs
@@ -12,8 +12,10 @@ namespace TARpv19
             Console.WriteLine("Please, enter your name:");
             string name = Console.ReadLine();
 
-            Console.WriteLine("Hello " + name + "!");
+            //Console.WriteLine("Hello " + name + "!");
 
+            //string interpolation
+            Console.WriteLine($"Hello, {name}!");
 
             Console.ReadLine();
         }
