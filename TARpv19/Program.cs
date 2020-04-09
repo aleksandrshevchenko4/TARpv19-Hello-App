28e9415db5a2ae2be51897fbdfe3b91e63dcd8c3
aleksandr.shevchenko <sanjkashevcenko@gmail.com>
2020-04-09 11:23:13 +0300

    Ask User Name

--- TARpv19/Program.cs
+++ TARpv19/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace TARpv19
 {
@@ -10,6 +9,13 @@ namespace TARpv19
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please, enter your name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name + "!");
+
+
+            Console.ReadLine();
         }
     }
 }
